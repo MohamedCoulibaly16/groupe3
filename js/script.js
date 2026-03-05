b9993b191033577aefb1ec9b4ff1faf710000f62
Morbin <supermorba@gmail.com>
2026-03-05 12:56:29 +0000

    nouvelle methode de téléchargement

--- js/script.js
+++ js/script.js
@@ -27,19 +27,68 @@ styles.textContent = `
 
 document.body.appendChild(toast);
 document.body.appendChild(styles)
+
+// function downloadPDF() {
+//   window.print()
+
+//   toast.style.top = "30px"
+
+//   setTimeout(() => {
+//     toast.style.top = "-80px"
+//   }, 2000)
+// }
+
 pdfModule = document.createElement('script')
 pdfModule.src = "https://cdnjs.cloudflare.com/ajax/libs/html2pdf.js/0.10.1/html2pdf.bundle.min.js"
 document.body.appendChild(pdfModule)
 
-function downloadPDF() {
-  window.print()
+// function downloadPDF() {
+
+//   const element = document.getElementById("content");
+
+//   const opt = {
+//     margin: 20,
+//     filename: "page.pdf",
+//     image: { type: "jpeg", quality: 0.98 },
+//     html2canvas: {
+//       scale: 2,
+//     },
+//     jsPDF: {
+//       unit: "mm",
+//       format: "a4",
+//       orientation: "portrait"
+//     }
+//   };
+
+//   html2pdf().set(opt).from(element).save();
+
+//   toast.style.top = "30px"
+
+//   setTimeout(() => {
+//     toast.style.top = "-80px"
+//   }, 2000)
+// }
+
+function downloadPDF(element, name) {
+
+  const path = element.dataset.docpath
+
+  const link = document.createElement("a");
+  link.href = path;      // chemin du fichier
+  link.download = name + '.pdf';  // nom pour le téléchargement
+  document.body.appendChild(link);
+  link.click();              // simule le clic
+  document.body.removeChild(link);
 
   toast.style.top = "30px"
 
   setTimeout(() => {
     toast.style.top = "-80px"
   }, 2000)
+
 }
+
+
 
 const message = document.getElementById("message");
 
