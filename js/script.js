3b02d2215b50623906e9ae3deb950270493343a0
ibrahima Coulibaly <boulby9023@gmail.com>
2026-03-05 15:00:48 +0000

    suppression console

--- js/script.js
+++ js/script.js
@@ -72,7 +72,6 @@ document.body.appendChild(pdfModule)
 function downloadPDF(element, name) {
 
   const path = element.dataset.docpath
-  console.log("Button clicked, path:", path);
   const link = document.createElement("a");
   link.href = path;      // chemin du fichier
   link.download = name + '.pdf';  // nom pour le téléchargement
